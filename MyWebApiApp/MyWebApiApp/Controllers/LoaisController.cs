f218fbf51e46373bc0751ac1be3550330889972b
Văn Mạnk <vmk28102k1@gmail.com>
2023-02-23 05:56:17 +0700

    study code api

---
+++ MyWebApiApp/MyWebApiApp/Controllers/LoaisController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Data;
+using MyWebApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public LoaisController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var dsLoai = _context.Loais.ToList();
+                return Ok(dsLoai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo => lo.MaLoai == id);
+            if (loai != null)
+            {
+                return Ok(loai);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CreateNew(LoaiModel model)
+        {
+            try
+            {
+                var loai = new Loai
+                {
+                    TenLoai = model.TenLoai
+                };
+                _context.Add(loai);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, loai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateLoaiById(int id, LoaiModel model)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo => lo.MaLoai == id);
+            if (loai != null)
+            {
+                loai.TenLoai = model.TenLoai;
+                _context.SaveChanges();
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLoaiById(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo => lo.MaLoai == id);
+            if (loai != null)
+            {
+                _context.Remove(loai);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Data/MyDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApiApp.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+
+
+        #region DbSet
+        public DbSet<HangHoa> HangHoas { get; set; }
+        public DbSet<Loai> Loais { get; set; }
+        public DbSet<DonHang> DonHangs { get; set; }
+        public DbSet<DonHangChiTiet> DonHangChiTiets { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DonHang>(e =>
+            {
+                e.ToTable("DonHang");
+                e.HasKey(dh => dh.MaDh);
+                e.Property(dh => dh.NgatDat).HasDefaultValueSql("getutcdate()");
+                e.Property(dh => dh.NguoiNhan).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<DonHangChiTiet>(entity =>
+            {
+                entity.ToTable("ChiTietDonHang");
+                entity.HasKey(e => new { e.MaDh, e.MaHh });
+
+                entity.HasOne(e => e.DonHang)
+                    .WithMany(e => e.DonHangChiTiets)
+                    .HasForeignKey(e => e.MaDh)
+                    .HasConstraintName("FK_DonHangCT_DonHang");
+
+                entity.HasOne(e => e.HangHoa)
+                    .WithMany(e => e.DonHangChiTiets)
+                    .HasForeignKey(e => e.MaHh)
+                    .HasConstraintName("FK_DonHangCT_HangHoa");
+            });
+        }
+    }
+}
